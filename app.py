b98cde32656fe1f4924b0b68fb637e361909b637
Natália <nataliacxavier1@gmail.com>
2019-10-31 15:54:43 -0400

    adding username

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
   
-from flask import Flask 
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app,cors_allowed_origins="*")
 
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
+@app.route('/')
+def sessions():
+    return render_template('index.html')
+	
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
 
 if __name__ == '__main__':
 	socketio.run(app)
