c35419493d3268ada95cdd58c68d0e7b40303a2c
Natália <nataliacxavier1@gmail.com>
2019-11-01 12:16:33 -0400

    get json response

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ socketio = SocketIO(app,cors_allowed_origins="*")
 @app.route('/')
 def sessions():
     return render_template('index.html')
-	
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+    socketio.emit('my response', json, callback=handle_json(json))
+
+def handle_json(json):
+    print('received json: ' + str(json))
 
 
 if __name__ == '__main__':
